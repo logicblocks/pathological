a6cd019dd238032a9389317a2ec8f0f337c96d56
Toby Clemson <tobyclemson@gmail.com>
2019-08-20 09:49:07 +0100

    Add test for reload on FileStore.

--- test/pathological/file_stores_test.clj
+++ test/pathological/file_stores_test.clj
@@ -4,13 +4,16 @@
 
     [pathological.file-stores :as fst]
     [pathological.file-systems :as fs]
+    [pathological.files :as f]
+    [pathological.paths :as p]
 
     [pathological.testing
      :refer [random-file-system-name
              new-in-memory-file-system
              unix-configuration
              windows-configuration]])
-  (:import [java.nio.file.attribute BasicFileAttributeView AclFileAttributeView]))
+  (:import [java.nio.file.attribute AclFileAttributeView
+                                    BasicFileAttributeView]))
 
 (deftest supports-file-attribute-view
   (testing "for keyword"
@@ -81,3 +84,24 @@
             file-store (first (fs/file-stores test-file-system))]
         (is (false? (fst/supports-file-attribute-view
                       file-store AclFileAttributeView)))))))
+
+(deftest reload
+  (testing "returns an updated file store"
+    (let [test-file-system
+          (new-in-memory-file-system
+            (random-file-system-name)
+            (unix-configuration
+              :attribute-views #{:basic :owner :posix}))
+
+          file-store-before (first (fs/file-stores test-file-system))
+          _ (f/write-lines (p/path test-file-system "/file")
+              ["Line 1" "Line 2"])
+          file-store-after (fst/reload file-store-before)]
+      (is (< (:usable-space file-store-after)
+            (:usable-space file-store-before)))
+      (is (< (:unallocated-space file-store-after)
+            (:unallocated-space file-store-before)))
+      (is (= (select-keys file-store-after
+               [:name :type :read-only? :total-space :block-size])
+            (select-keys file-store-before
+              [:name :type :read-only? :total-space :block-size]))))))
