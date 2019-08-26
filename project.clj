1f2a5da3fb08c0bd6f4fcff92bab17655b55ee68
Toby Clemson <tobyclemson@gmail.com>
2019-08-26 22:36:37 +0100

    Version 0.1.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/pathological "0.1.8"
+(defproject io.logicblocks/pathological "0.1.9-SNAPSHOT"
   :description "A thin but complete wrapper of Java NIO2 for Clojure."
   :url "https://github.com/logicblocks/pathological"
 
