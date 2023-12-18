bf73b077d42f54bfbbc610f5601828c6147f39e7
Circle CI <circleci@logicblocks.io>
2023-12-18 00:04:56 +0000

    Pre-release version 0.1.22-RC3 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/pathological "0.1.22-RC2"
+(defproject io.logicblocks/pathological "0.1.22-RC3"
   :description "A thin but complete wrapper of Java NIO2 for Clojure."
   :url "https://github.com/logicblocks/pathological"
 
